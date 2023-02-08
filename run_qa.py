44341f39a9f51a226931d4fc04cf5875b5bda927
World (Gi-hyeon) <simworld415@gmail.com>
2023-02-08 18:49:10 +0900

    Update run_qa.py

--- run_qa.py
+++ run_qa.py
@@ -628,7 +628,7 @@ def main():
             cache_dir=model_args.cache_dir,
             revision=model_args.model_revision,
             use_auth_token=True if model_args.use_auth_token else None,
-            args=training_args,
+            model_args=training_args,
         )
         if training_args.do_train:
             training_dataset = model.prepare_tf_dataset(
